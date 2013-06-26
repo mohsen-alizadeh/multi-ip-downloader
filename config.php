787bf450a7608fc047c8b78bcfcec88f8751e661
mohsen alizadeh <0x21hate@gmail.com>
2013-06-26 20:54:13 +0430

    set configuration properties

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+
+# directory to save downloaded files  
+define("FILES_DIR", "Downloads/");
+
+# list of your ip addresses
+# you should add this ip list in your network configuration
+# manual : http://code.seanodonnell.com/?id=2
+
+$ip[]	=	'4.2.2.4';
+$ip[]	=	'8.8.8.8';
