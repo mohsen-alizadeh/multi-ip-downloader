f695aabf9f7cb54ee92e1a2cc56036f9b7b729ef
mohsen alizadeh <0x21hate@gmail.com>
2013-06-26 22:33:36 +0430

    declare is_support_range_download #2

--- functions.php
+++ functions.php
@@ -10,3 +10,27 @@ function get_remote_file_size($url)
 	curl_exec($ch);
 	return curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
 }
+
+function is_support_range_download($url)
+{
+	$ch = curl_init($url);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_NOBODY, true);
+
+	$headerString = curl_exec($ch);
+
+	$headersArray = explode("\r\n", $headerString);
+
+	foreach($headersArray as $header)
+	{
+		$headerArray = explode(":", $header) ;
+
+		if(isset($headerArray[0]) &&
+				strtolower(trim(($headerArray[0])))== strtolower('Accept-Ranges'))
+			return true;
+	}
+
+	return false;
+}
