b3bcef4fc63f69b74300d461b3ce58ae83660d51
mohsen alizadeh <0x21hate@gmail.com>
2013-07-04 18:57:48 +0430

    add new line constant to configuration file

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php 
 
 # directory to save downloaded files  
-define("FILES_DIR", "Downloads/");
+define("FILES_DIR", "/root/");
 
 # list of your ip addresses
 # you should add this ip list in your network configuration
 # manual : http://code.seanodonnell.com/?id=2
 
-$ip[]	=	'4.2.2.4';
-$ip[]	=	'8.8.8.8';
+$ip[]	=	'213.233.173.223';
+$ip[]	=	'213.233.173.223';
+
+
+define("NL", "\n");
