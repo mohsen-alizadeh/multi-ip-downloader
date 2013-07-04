1c61165ca7e42951bc98313b17ae91b1b67b3c1a
mohsen alizadeh <0x21hate@gmail.com>
2013-07-04 19:06:59 +0430

    add project follow comments

--- multidl.php
+++ multidl.php
@@ -4,31 +4,43 @@
 include_once 'config.php';
 include_once 'functions.php';
 
+echo "project started" . NL;
 
 $url = $argv[1];
 $output = basename($url);
 
+echo "url : $url" .NL;
+echo "output: $output" . NL;
+
 if(!is_support_range_download($url))
 	die("link does`nt support range download\n");
 
+echo "url supports download range".NL;
 
 $file_size = get_remote_file_size($url);
+
+echo "file size : $file_size" . NL;
+
 $ip_count = count($ip);
+
+echo "ip count : $ip_count" . NL;
+
 $sizes = split_file_size($file_size, $ip_count);
 
 
 for($i=0; $i < $ip_count; $i++)
 {
-
 	$pid = pcntl_fork();
 	if ($pid == -1) {
 		die('could not fork');
 	} else if ($pid) {
 		// we are the parent
-		pcntl_wait($status); //Protect against Zombie children
+		//pcntl_wait($status); //Protect against Zombie children
 	} else {
 		// we are at the child
+		echo "starting download_range($url, $ip[$i], " . $sizes[$i]['start'] . " , " . $sizes[$i]['end'] . ")" . NL;
 		$data = download_range($url, $ip[$i], $sizes[$i]['start'], $sizes[$i]['end']);
+		echo "finished download_range($url, $ip[$i], " . $sizes[$i]['start'] . " , " . $sizes[$i]['end'] . ")" . NL;
 		file_put_contents(FILES_DIR . $output . '.' . $i, $data);
 		die();
 	}
